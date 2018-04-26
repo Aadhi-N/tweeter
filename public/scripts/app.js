80de0c65c6532c82426fd70e48b1a64460853069
Fathima-N <aadhilah.nizamdeen@gmail.com>
2018-04-26 11:27:43 -0400

    more updates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,30 @@
 $(document).ready(function() {
-  
+  $("button").click(function() {
+    $(".new-tweet").slideToggle("fast");
+    $("textarea").focus();
+  });
 
-  $("form").submit(function(event) {
+ $("form").submit(function(event) {
     event.preventDefault();
-
-    $.ajax({
-      type: "POST",
-      url: "/tweets",
-      data: $(this).serialize(),
-      success: function(data) {
-        loadTweets();
-      }
-
-    });
+    let textarea = $("form textarea");
+    if (textarea.val() == "" || textarea.val() == null) {
+      alert("Please enter some text!");
+    } else if (textarea.val().length > 140) {
+      alert("Your comment is too long!")
+    } else {
+      $.ajax({
+        type: "POST",
+        url: "/tweets",
+        data: $(this).serialize(),
+        success: function(data) {
+          $("main .container").empty();
+          $("textarea").val("");
+          loadTweets(); //this makes the new tweet show up on the page. 
+        }
+      }); //ajax bracket
+    }
   }); //bracket for form.submit
+
 
   function loadTweets() {
       $.ajax({
@@ -37,7 +48,7 @@ function createTweetElement (tweet) {
     <h2>${tweet.user.name}</h2>
     <span class="handle">${tweet.user.handle}</span>
     </header>
-    <p>${tweet.content.text}</p>
+    <p>${escape(tweet.content.text)}</p>
     <footer>
     <i class="fas fa-heart"></i>
     <i class="fas fa-share-alt"></i>
@@ -51,11 +62,23 @@ function createTweetElement (tweet) {
 
 };
 
-function renderTweets(tweets) {
-  for (var i = 0; i < tweets.length; i++) {
-    var $tweet = createTweetElement(tweets[i]);
-    $('#tweets-container').append($tweet);
-  }
+
+function escape(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
 }
 
+function renderTweets(tweets) {
+  for (var i = 0; i < tweets.length; i++) {
+   // for (var i = tweets.length; i > 0; --i) {
+    var $tweet = createTweetElement(tweets[i]);
+    $('#tweets-container').prepend($tweet);
+  }
+};
 
+// function convertDate() {
+//   var unixTime = ${tweet.created_at};
+//   var day = unixTime.getDate();
+//   console.log(day);
+// }
